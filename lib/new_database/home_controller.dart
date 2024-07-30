ad8fe2dcebb1cfe62dc39cae202d20134f59961b
Admin <maayush235@gmail.com>
2024-07-30 13:51:29 +0530

    Radhey Radhey

--- lib/new_database/home_controller.dart
+++ lib/new_database/home_controller.dart
@@ -1,29 +1,29 @@
-
-
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import 'datadb_hellper.dart';
 import 'home_model.dart';
 
-class HomeController extends GetxController{
-
+class HomeController extends GetxController {
   RxList<User> userList = <User>[].obs;
 
   @override
-  void onInit(){
+  void onInit() {
     super.onInit();
 
     fetchUser();
-
   }
 
   Future<void> fetchUser() async {
     final data = await DbHelper.dbHelper.fetchData();
-    userList.value = data.map((e) => User.fromMap(e),).toList();
+    userList.value = data
+        .map(
+          (e) => User.fromMap(e),
+        )
+        .toList();
   }
 
-  void addUser(String name,int age,Uint8List photo){
+  void addUser(String name, int age, Uint8List photo) {
     DbHelper.dbHelper.insertData(name, age, photo);
     fetchUser();
   }
